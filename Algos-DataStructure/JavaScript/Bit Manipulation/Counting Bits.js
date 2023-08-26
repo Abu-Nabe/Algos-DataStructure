f150ae067827d88ed74ec873099d9f45b4c4684e
Abu <118349590+shadowfsdev@users.noreply.github.com>
2023-08-26 11:05:02 +1000

    Add files via upload

---
+++ Algos-DataStructure/JavaScript/Bit Manipulation/Counting Bits.js
@@ -0,0 +1,12 @@
+Counting Bits
+
+var countBits = function (n, dp = [ 0 ]) {
+    for (let i = 1; i < (n + 1); i++) {
+        const [ mid, bit ] = [ (i >> 1), (i & 1) ]
+        const bits = (dp[mid] + bit)
+
+        dp.push(bits);
+    }
+
+    return dp;
+};
